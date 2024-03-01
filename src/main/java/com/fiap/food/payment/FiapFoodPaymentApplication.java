03b2e7d85d94f4200ece7302c454bc74f54a1f55
guilherme.novais <guilher.novais@shopper.com.br>
2024-02-29 22:50:53 -0300

    implementando lib mercado pago

--- src/main/java/com/fiap/food/payment/FiapFoodPaymentApplication.java
+++ src/main/java/com/fiap/food/payment/FiapFoodPaymentApplication.java
@@ -1,4 +1,4 @@
-package com.fiap.food.payment.fiapfoodpayment;
+package com.fiap.food.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/fiap/food/payment/FiapFoodPaymentApplicationTests.java
+++ src/test/java/com/fiap/food/payment/FiapFoodPaymentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fiap.food.payment.fiapfoodpayment;
+package com.fiap.food.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
